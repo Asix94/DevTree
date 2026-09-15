18be3a74b3c619f5f471ad7e7ff9a6f8bb5ca100
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-15 17:28:12 +0200

    añadiendo el handle para las cuentas

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import slug from 'slug';
 import User from "../models/User";
 import { hashPassword } from '../utils/auth';
 
@@ -9,12 +10,21 @@ export const createAcount = async (req: Request, res: Response) => {
     const userExists = await User.findOne({email});
 
     if(userExists) {
-        const error = new Error('El usuario ya esta registrado');
+        const error = new Error('Un usuario con ese email ya esta registrado');
         return res.status(409).json({error: error.message});
     }
 
+    const handle = slug(req.body.handle, '');
+    const handleExists = await User.findOne({handle});
+    if(handleExists) {
+        const error = new Error('Nombre de usuario no disponible');
+        return res.status(409).json({error: error.message});
+    }
+
+
     const user = new User(req.body); 
     user.password = await hashPassword(password);
+    user.handle = handle;
 
     await user.save();
 
