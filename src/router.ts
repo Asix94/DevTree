98a1380bd5ec2eb5d4d0bd73aced1c42f97b9627
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-15 15:35:09 +0200

    retornar mensajes de respuesta cuando un usuario de registra

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import User from './models/User';
 
 const router = Router();
 
 /** Autenticacion y registro */
-router.post('/auth/register', (req, res) => {
-    console.log(req.body);
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body); 
+
+    await user.save();
+
+    res.send('Registro creado correctamnete');
 });
 
 export default router;
