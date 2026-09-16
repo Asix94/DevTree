3817b68d6606ca2ff72be739d32fbfdafaadd977
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 10:26:24 +0200

    validacion con express validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import slug from 'slug';
 import User from "../models/User";
 import { hashPassword } from '../utils/auth';
 
 export const createAcount = async (req: Request, res: Response) => {
 
-    const { email, password } = req.body
+    // Manejar errores
+    let errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const { email, password } = req.body;
 
     const userExists = await User.findOne({email});
 
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createAcount } from './handlers';
 
 const router = Router();
 
 /** Autenticacion y registro */
-router.post('/auth/register', createAcount);
+router.post('/auth/register', 
+    body('handle').notEmpty(),
+    createAcount);
 
 export default router;
