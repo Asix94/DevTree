50c78cbd7858a32c7fb70fba62475192e4b6002e
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-16 10:33:21 +0200

    añadiendo el resto de validaciones al registro

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,18 @@ const router = Router();
 
 /** Autenticacion y registro */
 router.post('/auth/register', 
-    body('handle').notEmpty(),
+    body('handle')
+        .notEmpty()
+        .withMessage('El Handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El Nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('E-mail no valido'),
+    body('name')
+        .isLength({min: 8})
+        .withMessage('El Password es muy corto minimo 8 caracteres'),
     createAcount);
 
 export default router;
