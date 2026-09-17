6e5b90388c81e29a10b6630cc89e541422b319ec
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-17 16:00:29 +0200

    identificacion del usuario por jwt

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import slug from 'slug';
+import jwt from 'jsonwebtoken';
 import User from "../models/User";
 import { checkPassword, hashPassword } from '../utils/auth';
 import { generateJWT } from '../utils/jwt';
@@ -60,5 +61,26 @@ export const getUser = async (req: Request, res: Response) => {
     if(!bearer) {
         const error = new Error('No Autorizado');
         return res.status(401).json({error: error.message})
+    }
+
+    const [, token] = bearer.split(' ');
+
+    if(!token) {
+        const error = new Error('No Autorizado');
+        return res.status(401).json({error: error.message})
+    }
+
+    try {
+        const result = jwt.verify(token, process.env.JWT_SECRET);
+        if(typeof result === 'object' && result.id) {
+            const user = await User.findById(result.id).select('-password');
+            if(!user) {
+                const error = new Error('El Usuario no existe');
+                return res.status(401).json({error: error.message})
+            }
+            res.json(user);
+        }
+    } catch(error) {
+        res.status(500).json({error: 'Token No Válido'})
     }
 }
