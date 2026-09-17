221eaedfd942dbeede427943e70ea5f2b7e1285c
Carlos Ramos Torres <carlosrt1994@gmail.com>
2026-09-17 16:20:08 +0200

    protegiendo el endpoint de usuario para que los usuarios autentificados accedan

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import slug from 'slug';
-import jwt from 'jsonwebtoken';
 import User from "../models/User";
 import { checkPassword, hashPassword } from '../utils/auth';
 import { generateJWT } from '../utils/jwt';
@@ -56,31 +55,5 @@ export const login = async (req: Request, res: Response) => {
 }
 
 export const getUser = async (req: Request, res: Response) => {
-    const bearer = req.headers.authorization
-
-    if(!bearer) {
-        const error = new Error('No Autorizado');
-        return res.status(401).json({error: error.message})
-    }
-
-    const [, token] = bearer.split(' ');
-
-    if(!token) {
-        const error = new Error('No Autorizado');
-        return res.status(401).json({error: error.message})
-    }
-
-    try {
-        const result = jwt.verify(token, process.env.JWT_SECRET);
-        if(typeof result === 'object' && result.id) {
-            const user = await User.findById(result.id).select('-password');
-            if(!user) {
-                const error = new Error('El Usuario no existe');
-                return res.status(401).json({error: error.message})
-            }
-            res.json(user);
-        }
-    } catch(error) {
-        res.status(500).json({error: 'Token No Válido'})
-    }
+    res.json(req.user);
 }
